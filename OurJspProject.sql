aa7594389a7c6f136470ae46c368e0d8adb48c72
eastar93 <eastar93@naver.com>
2021-08-27 22:52:59 +0900

    eastar93

--- OurJspProject.sql
+++ OurJspProject.sql
@@ -4,42 +4,34 @@ CREATE TABLE users(
     uname varchar(10) not null,
 	uemail varchar(45) not null,
     subject varchar(50),
-    htmlcount INT,
-    csscount INT,
-    javacount INT,
-    jspcount INT
+    qcode integer DEFAULT 0,
+    point integer DEFAULT 0,
+    tier integer DEFAULT 0
     );
 
 INSERT into users VALUES('test', '1234', 'user', 'user@test.com', 'html, java', 0, 0, 0, 0);
 
 SELECT * from users;
 
-DROP TABLE users;
+DROP TABLE answer;
     
 create table question(
     qcode integer primary key,
-    question varchar(60)
+    question varchar(60),
+    qtcount integer DEFAULT 0 
+);
+
+CREATE TABLE solve (
+  auto_acode INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  qcode INT NOT NULL,
+  answer VARCHAR(30) NULL,
+  solvedate DATETIME NOT NULL
 );
 
 create table correct(
-    ccode integer auto_increment primary key,
+    ccode integer primary key,
     correct varchar(30)
 );
-    
-CREATE TABLE project.answer (
-  acode INT NOT NULL,
-  accacode INT NOT NULL,
-  anuid VARCHAR(20) NOT NULL,
-  answer VARCHAR(30) NULL,
-  solvedate DATETIME NOT NULL,
-  PRIMARY KEY (acode),
-  INDEX anuid_idx (anuid ASC) VISIBLE,
-  CONSTRAINT anuid
-    FOREIGN KEY (anuid)
-    REFERENCES project.users (uid)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
     
 CREATE TABLE board (
 	  no INT primary key not null AUTO_INCREMENT,
